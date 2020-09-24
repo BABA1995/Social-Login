0b20696bbafa2908cd36dc860614acd3d2d15599
BABA 1995 <fakruddinbaba16@gmail.com>
2020-09-24 16:19:20 +0530

    firstcommit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,21 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
 export const environment = {
   production: false
 };
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxQ8PlmcbFy05hB9fA9mYou7rAoqqwEtg",
+  authDomain: "fir-demo-fa63c.firebaseapp.com",
+  databaseURL: "https://fir-demo-fa63c.firebaseio.com",
+  projectId: "fir-demo-fa63c",
+  storageBucket: "fir-demo-fa63c.appspot.com",
+  messagingSenderId: "834802844152",
+  appId: "1:834802844152:web:46374cdf665284def9d60d",
+  measurementId: "G-QTB7THCYVL"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
